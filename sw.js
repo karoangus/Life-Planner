8a749de653022cc1acc7466c07e4d048bb308b14
karoangus <karoangus@gmail.com>
2026-08-16 10:08:30 +0330

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'life-planner-cache-v16';
+const CACHE_NAME = 'life-planner-cache-v17';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
